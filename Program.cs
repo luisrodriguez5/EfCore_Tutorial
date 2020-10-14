7ffa66a8bc3873de9caa888ffd066d82cfea5ecb
Luis <luis_rodriguez5@ucne.edu.do>
2020-10-14 13:02:03 -0400

    Carga inicial del proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCore_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
